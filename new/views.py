6f62fe178a34ad356ff154f73614ee70cf9e4264
yeiad <kabidyeiad101@gmail.com>
2022-09-09 21:40:42 +0600

    new tabe added

--- new/views.py
+++ new/views.py
@@ -1,5 +1,3 @@
-import email
-from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Contact
@@ -17,11 +15,13 @@ def contacts(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
+        house = request.POST.get('message')
         contact = Contact() #creating Contact object as contact
 
         contact.name = name #contact.name means im calling Contact classes  name
         contact.email = email
         contact.message = message
+        contact.house = house
         contact.save() # saving data
         return HttpResponse('<h1> Valar Morghulis </h1>')
 
