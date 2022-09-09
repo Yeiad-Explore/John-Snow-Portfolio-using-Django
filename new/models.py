d96b7abe6df93be91db00f03c5927fc381aac3c0
yeiad <kabidyeiad101@gmail.com>
2022-09-09 20:51:01 +0600

    database added

--- new/models.py
+++ new/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100,null=True) 
+    email= models.EmailField(max_length=100,null=True)
+    message= models.TextField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
+
--- new/views.py
+++ new/views.py
@@ -1,5 +1,8 @@
+import email
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
 # Create your views here.
 
@@ -10,4 +13,16 @@ def about(request):
     return render(request, 'about.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact() #creating Contact object as contact
+
+        contact.name = name #contact.name means im calling Contact classes  name
+        contact.email = email
+        contact.message = message
+        contact.save() # saving data
+        return HttpResponse('<h1> Valar Morghulis </h1>')
+
     return render(request,'contacts.html')
